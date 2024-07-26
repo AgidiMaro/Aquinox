ef6db33a1d113d7acd11244c2c3e5f875b381317
AgidiMaro <agidioghenemaro@gmail.com>
2024-07-26 06:57:10 +0100

    Update Excel Report

--- next-audit-python/auditllm.py
+++ next-audit-python/auditllm.py
@@ -20,7 +20,7 @@ Question :
 def prompt_for_details_prefix(additional_context: str, example:str) -> str:
     example_text = f"\nUse the following example to guide your tone and structure but do not use data from the example:\n{example}" if example else ""
     return f"""
-    You are a PwC IT audit team documenting the design and implementation of IT controls in a client enviroment. This will be the documentation kept in the audit file. 
+    You are a PwC IT audit team documenting the audit of the design and implementation of IT controls. This will be the documentation kept in the audit file. 
     Additional Context: {additional_context} 
     Provide a detailed response to the following question based on best practices for IT change management. Include as much context and specific information as possible.
     {example_text}
@@ -75,13 +75,16 @@ class AuditLLM:
             result[domain] = domain_list
 
             # Iterate through each question in the domain
-            for domain_question in domain_questions:
+            for domain_question_data in domain_questions:
+                domain_question = domain_question_data["question"]
+                example = domain_question_data["example"]
+
                 # Perform similarity search for the question
                 docs = knowledge_base.similarity_search(domain_question)
                 # Generate details using the QA chain. This is returns the finding/details column of the final output
 
                 
-                prompt_for_details = prompt_for_details_prefix(additional_context) + f"Question: {domain_question}\n"
+                prompt_for_details = prompt_for_details_prefix(additional_context,"") + f"Question: {domain_question}\n"
                 with get_openai_callback() as cb:
                     details = self.chain.run(input_documents=docs, question=prompt_for_details)
                 
